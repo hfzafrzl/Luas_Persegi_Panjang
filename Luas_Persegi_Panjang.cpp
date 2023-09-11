c9b71047fdc4d646cd1961ead04d49179472b5c0
hfzafrzl <144640775+hfzafrzl@users.noreply.github.com>
2023-09-11 09:24:31 +0700

    Create Luas_Persegi_Panjang.cpp

---
+++ Luas_Persegi_Panjang.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main( ) {
+     int h, p, l ;
+     cout << "MENGHITUNG LUAS PERSEGI PANJANG \n";
+     cout<<"=========================\n";
+     cout << "masukan sisi persegi panjang: ";
+     cin >>p ;
+     cout<<"masukan lebar persegi panjang:";
+     cin>>l;
+    h = p*l ; 
+     cout<< " hasil  persegi adalah: "<<h<<endl;
+}
